3ece53a7d7664e55888ef8f6bd7d9d35dee154bd
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-05-22 12:30:57 +0200

    refactor: add provideZoneChangeDetection

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,13 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideZoneChangeDetection(), provideRouter([])],
     }).compileComponents();
   });
 
@@ -16,8 +19,9 @@ describe('App', () => {
 
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hive');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hive');
   });
 });
