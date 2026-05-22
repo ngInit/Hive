6e64dae297e45ffe0ee0e517c4a6458c2d0cda4d
ngInit <122630180+ngInit@users.noreply.github.com>
2026-05-22 20:40:27 +0200

    feat: add main-layout (#10)
    * feat: create main-layout component
    
    * refactor: add header and footer to main-layout component
    
    * refactor: add styles for main-layout component
    
    * feat: add main-layout to app

---
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Header } from '@core/layouts/header/header';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from '@core/layouts/footer/footer';
+
+@Component({
+  selector: 'hive-main-layout',
+  imports: [Header, RouterOutlet, Footer],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainLayout {
+  userAuthorization = false;
+  title = input.required<string>();
+}
