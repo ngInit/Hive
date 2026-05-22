88ca1f11b2faf5992ccc04404facbad0a84fcd50
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-05-22 19:49:13 +0200

    refactor: add content to header

--- src/app/core/layouts/header/header.ts
+++ src/app/core/layouts/header/header.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'hive-header',
-  imports: [],
+  imports: [RouterLink, FormsModule, MatButton, MatIconButton, MatIcon],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
-  appName = 'Hive';
+  readonly isAuthorized = input.required<boolean>();
+  protected searchPlaceholder = 'Find a song and more...';
+  protected searchInput = '';
 }
