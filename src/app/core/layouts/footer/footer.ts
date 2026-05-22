3d17379cc1867505dfc6283e790432c718caf7a4
ngInit <122630180+ngInit@users.noreply.github.com>
2026-05-22 17:08:15 +0200

    feat: add footer (#6)

---
+++ src/app/core/layouts/footer/footer.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer {
+  protected readonly copyrightText = `© ${String(new Date().getFullYear())} Y2K. All Rights Reserved`;
+}
