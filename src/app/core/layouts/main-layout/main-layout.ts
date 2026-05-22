8f2bcb7db01e13299b17156ea5275b8546610259
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-05-22 20:06:40 +0200

    refactor: add header and footer to main-layout component

--- src/app/core/layouts/main-layout/main-layout.ts
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Header } from '@core/layouts/header/header';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from '@core/layouts/footer/footer';
 
 @Component({
   selector: 'hive-main-layout',
-  imports: [],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainLayout {
-  title = 'main-layout works!';
+  userAuthorization = false;
+  title = input.required<string>();
 }
