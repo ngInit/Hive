d5f005ca331adbf6acb1cc3f3334b2a379d52173
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-05-29 22:16:11 +0200

    feat: add content to track card component

--- src/app/shared/components/track-card/track-card.ts
+++ src/app/shared/components/track-card/track-card.ts
@@ -1,12 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { CardTrack } from '@core/models/track.model';
+
+const track: CardTrack = {
+  track_id: 0,
+  artist_id: 0,
+  artist_name: "Siren's Embrace",
+  track_name: 'Blood is My Crown',
+  track_cover: 'https://m.media-amazon.com/images/I/51Q+Utavr1L._UX358_FMwebp_QL85_.jpg',
+  track_duration: '4:50',
+  track_plays: '1.7M plays',
+};
 
 @Component({
   selector: 'hive-track-card',
-  imports: [],
+  imports: [MatIcon, RouterLink],
   templateUrl: './track-card.html',
   styleUrl: './track-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TrackCard {
-  title = 'track-card works!';
+  playIcon = 'play_circle_outline';
+  pauseIcon = 'pause_circle_outline';
+  protected track: CardTrack = track;
 }
