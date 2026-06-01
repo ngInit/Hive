f2d1fa20dcc92e875a92eb18b427a5fcebcd371c
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-02 01:00:40 +0200

    feat: add provider for repositories

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRepositories } from '@core/providers/repository.providers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    ...provideRepositories(),
   ],
 };
