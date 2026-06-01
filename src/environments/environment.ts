2445c01b6ed24702afb1e59f2d9f2d3a93718a93
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-02 00:25:40 +0200

    feat: add environments for prod and dev

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  appUrl: 'http://localhost:4200',
+  jamendoApiUrl: 'https://api.jamendo.com/v3.0',
+  jamendoClientId: '69fceb9a',
+  firebase: {
+    projectId: '..',
+    authDomain: '..',
+    apiKey: '...',
+  },
+  enableMockData: true,
+  mockDelayMs: 300,
+};
