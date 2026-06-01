526cdc01f600e18dcd926165cb922546342267cd
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-01 23:16:11 +0200

    feat: add short format pipes to the track card

--- src/app/shared/components/track-card/track-card.ts
+++ src/app/shared/components/track-card/track-card.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { Track } from '@core/models/track.model';
+import { TrackPlaysShortPipe } from '@shared/pipes/track-plays-short-pipe';
+import { TrackDurationShortPipe } from '@shared/pipes/track-duration-short-pipe';
 
 @Component({
   selector: 'hive-track-card',
-  imports: [MatIcon, RouterLink],
+  imports: [MatIcon, RouterLink, TrackPlaysShortPipe, TrackDurationShortPipe],
   templateUrl: './track-card.html',
   styleUrl: './track-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
