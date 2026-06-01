e3cc1a8b6d59b391b899613d6a08d31a81afa7fa
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-01 23:13:31 +0200

    feat: add pipe for short plays format

---
+++ src/app/shared/pipes/track-plays-short-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TrackPlaysShortPipe } from './track-plays-short-pipe';
+
+describe('TrackPlaysShortPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TrackPlaysShortPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
