af39d280e04fda84a496287c1e7c63bbf9195e93
ngInit <122630180+ngInit@users.noreply.github.com>
2026-06-02 01:09:07 +0200

    feat: add track service (#44)
    * feat: add tracks mock data
    
    * feat: add environments for prod and dev
    
    * feat: add TracksRepository abstraction with dependency injection token
    
    * feat: add MockTracksRepository
    
    * feat: add JamendoTracksRepository
    
    * feat: add delay utility for fake delays in mock repository
    
    * feat: add track service
    
    * feat: add provider for repositories
    
    * refactor: add aliases for environments
    
    * refactor: add configuration for production environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  appUrl: 'https://hive-eta-fawn.vercel.app/',
+  jamendoApiUrl: 'https://api.jamendo.com/v3.0',
+  jamendoClientId: '69fceb9a',
+  firebase: {
+    projectId: '..',
+    authDomain: '..',
+    apiKey: '...',
+  },
+  enableMockData: false,
+};
