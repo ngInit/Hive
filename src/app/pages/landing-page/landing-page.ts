96eb8d3cbbb4122b6bcc5267a51fb2960fcd34b6
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-02 16:30:34 +0200

    feat: add content to the landing page

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TagsPanel } from '@components/tags-panel/tags-panel';
+import { Carousel } from '@components/carousel/carousel';
 
 @Component({
   selector: 'hive-landing-page',
-  imports: [],
+  imports: [TagsPanel, Carousel],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
