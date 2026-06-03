1e68c4679775709bbcdf1251b60abb32880573a4
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-03 21:17:13 +0200

    feat: add content to the user page

--- src/app/pages/user-page/user-page.ts
+++ src/app/pages/user-page/user-page.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'hive-user-page',
-  imports: [],
+  imports: [MatButton],
   templateUrl: './user-page.html',
   styleUrl: './user-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserPage {
-  title = 'user-page works!';
+  title = 'User page... Will be later =)))';
+  private readonly router = inject(Router);
+
+  async signOut() {
+    await this.router.navigate(['/']);
+  }
 }
