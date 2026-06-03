f45a1b369d11d884e7b16c7893c9d56fdc1a4b75
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-03 23:02:32 +0200

    refactor: add route to the landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from '@pages/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPage,
+    title: 'Hive',
+  },
+];
