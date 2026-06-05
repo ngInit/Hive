0fa23df7148a2a82c429010f336561c268adc2eb
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-05 22:45:08 +0200

    feat: add content to the popup

--- src/app/shared/components/popup/popup.ts
+++ src/app/shared/components/popup/popup.ts
@@ -1,12 +1,60 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DOCUMENT,
+  HostBinding,
+  inject,
+  input,
+  output,
+  model,
+  effect,
+} from '@angular/core';
+import { MatActionList, MatListItem } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'hive-popup',
-  imports: [],
+  imports: [MatActionList, MatListItem, RouterLink],
   templateUrl: './popup.html',
   styleUrl: './popup.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Popup {
-  title = 'popup works!';
+  readonly open = model.required<boolean>();
+  readonly userStatus = input.required<boolean>();
+  readonly document = inject(DOCUMENT);
+  readonly signOut = output();
+
+  constructor() {
+    effect((onCleanup) => {
+      if (!this.open()) {
+        return;
+      }
+      const pause = setTimeout(() => {
+        this.document.addEventListener('click', this.closePopup);
+      }, 10);
+
+      onCleanup(() => {
+        clearTimeout(pause);
+        this.document.removeEventListener('click', this.closePopup);
+      });
+    });
+  }
+
+  @HostBinding('class.opened') get isOpened(): boolean {
+    return this.open();
+  }
+
+  @HostBinding('class.user') get isUser(): boolean {
+    return this.userStatus();
+  }
+
+  closePopup = (): void => {
+    this.open.set(false);
+  };
+
+  setSignOut(): void {
+    this.signOut.emit();
+    this.closePopup();
+  }
 }
