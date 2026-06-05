6cd08535d07f67db3d441003891ffdf69ab71f8c
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-05 22:57:41 +0200

    feat: add the popup with new functionality

--- src/app/core/layouts/header/header.ts
+++ src/app/core/layouts/header/header.ts
@@ -1,13 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { AuthService } from '@core/services/auth.service';
+import { Popup } from '@components/popup/popup';
 
 @Component({
   selector: 'hive-header',
-  imports: [RouterLink, FormsModule, MatButton, MatIconButton, MatIcon],
+  imports: [RouterLink, FormsModule, MatButton, MatIconButton, MatIcon, Popup],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,7 +17,12 @@ export class Header {
   protected searchPlaceholder = 'Find a song and more...';
   protected searchInput = '';
 
+  isPopupOpened = signal(false);
+
   signIn(): void {
     this.authService.logIn();
+  }
+  openPopup() {
+    this.isPopupOpened.update((state) => !state);
   }
 }
