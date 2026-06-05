1207234d7fc13ec52e97ac3f1e5af250ce822cdf
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-05 22:58:24 +0200

    refactor: add signOut method

--- src/app/core/layouts/header/header.ts
+++ src/app/core/layouts/header/header.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
@@ -13,6 +14,7 @@ import { Popup } from '@components/popup/popup';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  private readonly router = inject(Router);
   protected readonly authService = inject(AuthService);
   protected searchPlaceholder = 'Find a song and more...';
   protected searchInput = '';
@@ -22,6 +24,14 @@ export class Header {
   signIn(): void {
     this.authService.logIn();
   }
+
+  signOut(): void {
+    this.authService.logOut();
+    if (this.router.url === '/user') {
+      void this.router.navigate(['/']);
+    }
+  }
+
   openPopup() {
     this.isPopupOpened.update((state) => !state);
   }
