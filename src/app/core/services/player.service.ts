506452d88f86b801164a28afa928e30c875c4e32
ngInit <122630180+ngInit@users.noreply.github.com>
2026-06-08 12:46:12 +0200

    feat: add a simple player service (#84)

---
+++ src/app/core/services/player.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, signal } from '@angular/core';
+import { Track } from '@core/models/track.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlayerService {
+  private readonly currentTrackId = signal<number | null>(null);
+  private readonly isPlaying = signal<boolean>(false);
+
+  readonly playingTrackId = this.currentTrackId.asReadonly();
+  readonly isPlayingTrack = this.isPlaying.asReadonly();
+
+  playTrack(track: Track): void {
+    this.currentTrackId.set(Number(track.id));
+    this.isPlaying.set(true);
+  }
+
+  pauseTrack(): void {
+    this.isPlaying.set(false);
+  }
+
+  toggle(track: Track): void {
+    if (this.currentTrackId() === Number(track.id)) {
+      this.pauseTrack();
+    } else {
+      this.playTrack(track);
+    }
+  }
+}
