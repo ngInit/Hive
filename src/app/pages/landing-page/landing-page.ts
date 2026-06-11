e0f404ffef9c38317b2890a8c8a6b0a9b28eea7c
ngInit <122630180+ngInit@users.noreply.github.com>
2026-06-11 11:43:19 +0200

    feat: add tracks to the page landing (#90)
    * feat: add the track service and popular tracks to the landing page
    
    * feat: add the track card to the carousel of the landing page

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,12 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, effect, computed } from '@angular/core';
 import { TagsPanel } from '@components/tags-panel/tags-panel';
 import { Carousel } from '@components/carousel/carousel';
+import { Track } from '@core/models/track.model';
+import { TrackCard } from '@components/track-card/track-card';
+import { TracksService } from '@core/services/tracks.service';
 
 @Component({
   selector: 'hive-landing-page',
-  imports: [TagsPanel, Carousel],
+  imports: [TagsPanel, Carousel, TrackCard],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingPage {}
+export class LandingPage {
+  readonly tracksService = inject(TracksService);
+  readonly popularTracks = signal<Track[]>([]);
+  readonly popularTracksEmpty = computed(() => {
+    return !this.tracksService.tracks.isLoading() && this.popularTracks().length === 0;
+  });
+
+  constructor() {
+    this.tracksService.searchTracks('David TMX');
+
+    effect(() => {
+      const trackResponse = this.tracksService.tracks.value();
+      if (trackResponse) {
+        this.popularTracks.set(trackResponse.results);
+      }
+    });
+  }
+}
