3e963cdbf5f73195b4ec79e8909e5e03e3bf7d4b
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-13 23:05:53 +0200

    feat: add auth states to auth guard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,13 +1,22 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { effect, inject } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
+  const router = inject(Router);
+  const redirect = () => router.createUrlTree(['/']);
 
-  if (!authService.isLoggedIn()) {
-    return inject(Router).createUrlTree(['/']);
-  }
-
-  return true;
+  return new Promise<boolean | UrlTree>((resolve) => {
+    if (authService.isAuthReady()) {
+      resolve(authService.isAuthenticated() ? true : redirect());
+      return;
+    }
+    const refEffect = effect(() => {
+      if (authService.isAuthReady()) {
+        refEffect.destroy();
+        resolve(authService.isAuthenticated() ? true : redirect());
+      }
+    });
+  });
 };
