fc693bce4ef161e697d85e3d54126ca21b9411f7
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-13 23:04:57 +0200

    feat: add auth repository provider

--- src/app/core/providers/repository.providers.ts
+++ src/app/core/providers/repository.providers.ts
@@ -1,5 +1,8 @@
 import { environment } from '@env/environment';
+import { AUTH_REPOSITORY } from '@core/repositories/auth/auth.repository';
 import { TRACKS_REPOSITORY } from '@core/repositories/tracks/tracks.repository';
+import { MockAuthRepository } from '@core/repositories/auth/mock-auth.repository';
+import { FirebaseAuthRepository } from '@core/repositories/auth/firebase-auth.repository';
 import { MockTracksRepository } from '@core/repositories/tracks/mock-tracks.repository';
 import { JamendoTracksRepository } from '@core/repositories/tracks/jamendo-tracks.repository';
 
@@ -10,6 +13,13 @@ export function provideTracksRepository() {
   };
 }
 
+export function provideAuthRepository() {
+  return {
+    provide: AUTH_REPOSITORY,
+    useClass: environment.enableMockData ? MockAuthRepository : FirebaseAuthRepository,
+  };
+}
+
 export function provideRepositories() {
-  return [provideTracksRepository()];
+  return [provideTracksRepository(), provideAuthRepository()];
 }
