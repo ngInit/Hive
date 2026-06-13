48f88d445428860af49891c8bfa1d49791e30310
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-13 18:00:06 +0200

    feat: add firebase signIn method for auth repository

---
+++ src/app/core/repositories/auth/firebase-auth.repository.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { AuthRepository } from '@core/repositories/auth/auth.repository';
+import { environment } from '@env/environment';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { Auth, getAuth } from 'firebase/auth';
+import { Firestore, getFirestore, doc, setDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, updateProfile as updateFirebaseProfile } from 'firebase/auth';
+import { SignUpData } from '@core/models/auth.model';
+import { UserAuth, userConverter } from '@core/models/user.model';
+import { throwFirebaseAuthError } from '@core/errors/firebase-auth.error';
+
+const fbApp: FirebaseApp = initializeApp(environment.firebase);
+const auth: Auth = getAuth(fbApp);
+const db: Firestore = getFirestore(fbApp);
+
+interface fbData {
+  uid: string;
+  displayName: string | null;
+  email: string | null;
+}
+
+@Injectable()
+export class FirebaseAuthRepository implements AuthRepository {
+  private getAuthData(firebaseUser: fbData): UserAuth {
+    if (!firebaseUser.email) {
+      throw new Error("Email doesn't exist");
+    }
+    return {
+      uid: firebaseUser.uid,
+      nickname: firebaseUser.displayName ?? 'unknown',
+      email: firebaseUser.email,
+    };
+  }
+
+  async signUp(data: SignUpData): Promise<UserAuth> {
+    try {
+      const credential = await createUserWithEmailAndPassword(auth, data.email, data.password);
+      await updateFirebaseProfile(credential.user, { displayName: data.nickname });
+      await credential.user.reload();
+      await setDoc(doc(db, 'users', credential.user.uid).withConverter(userConverter), {
+        uid: credential.user.uid,
+        nickname: data.nickname,
+        email: data.email,
+      });
+      return this.getAuthData(credential.user);
+    } catch (error) {
+      throwFirebaseAuthError(error);
+    }
+  }
+}
