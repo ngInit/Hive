9c66a78d1daeaee1423583a9a1200a85fe46475b
ngInit <122630180+ngInit@users.noreply.github.com>
2026-06-14 09:18:53 +0200

    feat: create sign page (#98)
    * feat: create sign page
    
    * feat: add sign page to the router
    
    * feat: add base styles for sign page

---
+++ src/app/pages/sign-page/sign-page.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignPage } from './sign-page';
+
+describe('SignPage', () => {
+  let component: SignPage;
+  let fixture: ComponentFixture<SignPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SignPage],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SignPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
