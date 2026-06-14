36d251d3d3318cd4e92db7927cf5025a4a81cf63
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-14 21:13:35 +0200

    feat: add sign-in and sign-up forms to sign page

--- src/app/pages/sign-page/sign-page.ts
+++ src/app/pages/sign-page/sign-page.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatError, MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface SignInGroup {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'hive-sign-page',
+  imports: [MatFormField, MatLabel, MatInput, ReactiveFormsModule, MatError, MatButton],
   templateUrl: './sign-page.html',
   styleUrl: './sign-page.scss',
 })
-export class SignPage {}
+export class SignPage {
+  readonly movePanel = signal<string>('');
+  readonly isSignInPanel = signal(true);
+
+  signInForm = new FormGroup<SignInGroup>({
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.minLength(8), Validators.required],
+    }),
+  });
+
+  signIn() {
+    console.log(this.signInForm.value);
+  }
+
+  toggle(): void {
+    if (this.movePanel() === '') {
+      this.movePanel.set('sign-up');
+      this.isSignInPanel.set(false);
+      return;
+    } else if (this.movePanel() === 'sign-up') {
+      this.movePanel.set('sign-in');
+      this.isSignInPanel.set(true);
+      return;
+    } else if (this.movePanel() === 'sign-in') {
+      this.movePanel.set('sign-up');
+      this.isSignInPanel.set(false);
+    }
+  }
+}
