393668cbdd8139b7330e683d8d0038f3bb3bde53
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-14 09:09:31 +0200

    feat: create sign page

---
+++ src/app/pages/sign-page/sign-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-sign-page',
+  templateUrl: './sign-page.html',
+  styleUrl: './sign-page.scss',
+})
+export class SignPage {}
