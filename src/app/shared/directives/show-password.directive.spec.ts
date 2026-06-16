233472dc3fb3bba8a6a9c95561436a66b21e5c5f
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-16 02:50:49 +0200

    feat: create a show password directive

---
+++ src/app/shared/directives/show-password.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowPassword } from './show-password.directive';
+
+describe('ShowPasswordDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowPassword();
+    expect(directive).toBeTruthy();
+  });
+});
