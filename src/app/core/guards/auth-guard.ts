644d72e47da473b2869b995ce47511bae88edca9
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-16 14:33:39 +0200

    refactor: change auth guard logic

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -2,21 +2,35 @@ import { CanActivateFn, Router, UrlTree } from '@angular/router';
 import { effect, inject } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
 
-export const authGuard: CanActivateFn = () => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
-  const redirect = () => router.createUrlTree(['/']);
-
-  return new Promise<boolean | UrlTree>((resolve) => {
-    if (authService.isAuthReady()) {
-      resolve(authService.isAuthenticated() ? true : redirect());
-      return;
-    }
+function afterAuthIsReady(
+  service: AuthService,
+  response: () => boolean | UrlTree
+): boolean | UrlTree | Promise<boolean | UrlTree> {
+  if (service.isAuthReady()) {
+    return response();
+  }
+  return new Promise((resolve) => {
     const refEffect = effect(() => {
-      if (authService.isAuthReady()) {
+      if (service.isAuthReady()) {
         refEffect.destroy();
-        resolve(authService.isAuthenticated() ? true : redirect());
+        resolve(response());
       }
     });
+  });
+}
+
+export const guestGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return afterAuthIsReady(authService, () => {
+    return authService.isAuthenticated() ? router.createUrlTree(['/']) : true;
+  });
+};
+
+export const userGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return afterAuthIsReady(authService, () => {
+    return authService.isAuthenticated() ? true : router.createUrlTree(['/']);
   });
 };
