fdd34ec65d76e438f4783adc7065e90e5909cc47
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-16 13:29:15 +0200

    fix: add withPreloading to router configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withViewTransitions, withPreloading, PreloadAllModules } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withXhr } from '@angular/common/http';
 import { provideRepositories } from '@core/providers/repository.providers';
@@ -7,7 +7,7 @@ import { provideRepositories } from '@core/providers/repository.providers';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, withViewTransitions(), withPreloading(PreloadAllModules)),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withXhr()),
     ...provideRepositories(),
