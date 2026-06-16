58908094b41720166ffdf9884d57d01095cbac2e
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-16 02:46:03 +0200

    feat: add header and footer components to sign layout

--- src/app/core/layouts/sign-layout/sign-layout.ts
+++ src/app/core/layouts/sign-layout/sign-layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '@core/layouts/header/header';
+import { Footer } from '@core/layouts/footer/footer';
 
 @Component({
   selector: 'hive-sign-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './sign-layout.html',
   styleUrl: './sign-layout.scss',
 })
