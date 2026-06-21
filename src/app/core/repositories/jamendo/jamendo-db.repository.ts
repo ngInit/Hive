2663875df85526f9de06936d86bf2f59f68e15b8
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 01:37:33 +0200

    refactor: add JamendoDBRepository for real data

---
+++ src/app/core/repositories/jamendo/jamendo-db.repository.ts
@@ -0,0 +1,43 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '@env/environment';
+import { JamendoRepository } from '@core/repositories/jamendo/jamendo.repository';
+import { EndPoint, JamendoEndpointRequest, JamendoEndpointResponse } from '@core/models/jamendo/jamendo.model';
+import { firstValueFrom } from 'rxjs';
+
+type HttpParamValue = string | number | (string | number)[] | boolean | undefined | null;
+
+@Injectable()
+export class JamendoDbRepository implements JamendoRepository {
+  private readonly http = inject(HttpClient);
+  private convertJamendoParams(query: Record<string, HttpParamValue>): HttpParams {
+    let params = new HttpParams();
+    for (const [key, value] of Object.entries(query)) {
+      if (value === undefined || value === null) {
+        continue;
+      }
+      if (Array.isArray(value)) {
+        const arraySeparator = key === 'order' ? ' ' : ',';
+        params = params.set(key, value.map(String).join(arraySeparator));
+      } else {
+        params = params.set(key, value);
+      }
+    }
+    return params;
+  }
+
+  async createRequest<T extends EndPoint>(
+    endpoint: T,
+    params: Omit<JamendoEndpointRequest[T], 'client_id' | 'format'>
+  ): Promise<JamendoEndpointResponse[T]> {
+    return firstValueFrom(
+      this.http.get<JamendoEndpointResponse[T]>(`${environment.jamendoApiUrl}/${endpoint}/`, {
+        params: this.convertJamendoParams({
+          client_id: environment.jamendoClientId,
+          format: 'json',
+          ...params,
+        }),
+      })
+    );
+  }
+}
