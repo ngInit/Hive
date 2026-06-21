56c461f4d5734bf240a586bc83fa7cead426f858
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 01:35:23 +0200

    refactor: add models for Jamendo models

--- src/app/core/models/jamendo/jamendo.model.ts
+++ src/app/core/models/jamendo/jamendo.model.ts
@@ -1,3 +1,9 @@
+import { ArtistsRequest, ArtistsResponse } from '@core/models/jamendo/artists.model';
+import { TracksRequest, TracksResponse } from '@core/models/jamendo/tracks.model';
+import { AlbumsRequest, AlbumsResponse } from '@core/models/jamendo/albums.model';
+
+export type EndPoint = 'artists' | 'tracks' | 'albums';
+
 export interface JamendoResponseHeaders {
   status: 'success' | 'failed';
   code: number;
@@ -6,6 +12,18 @@ export interface JamendoResponseHeaders {
   results_count?: number;
   results_fullcount?: number;
   next?: string;
+}
+
+export interface JamendoEndpointRequest {
+  artists: ArtistsRequest;
+  tracks: TracksRequest;
+  albums: AlbumsRequest;
+}
+
+export interface JamendoEndpointResponse {
+  artists: ArtistsResponse;
+  tracks: TracksResponse;
+  albums: AlbumsResponse;
 }
 
 interface JamendoSuccessResponse<T> {
