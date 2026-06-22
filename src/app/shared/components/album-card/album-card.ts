e7b4ea7f6e8a8bb64e39e78f8db5633b8c551015
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 21:32:09 +0200

    feat: add router navigation to album

--- src/app/shared/components/album-card/album-card.ts
+++ src/app/shared/components/album-card/album-card.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Album } from '@core/models/jamendo/albums.model';
 
 @Component({
@@ -7,5 +8,14 @@ import { Album } from '@core/models/jamendo/albums.model';
   styleUrl: './album-card.scss',
 })
 export class AlbumCard {
+  private readonly router = inject(Router);
   readonly album = input.required<Album>();
+
+  async goToArtist(): Promise<void> {
+    await this.router.navigate(['/artist'], { queryParams: { q: this.album().artist_id } });
+  }
+
+  async goToAlbum(): Promise<void> {
+    await this.router.navigate(['/album'], { queryParams: { q: this.album().id } });
+  }
 }
