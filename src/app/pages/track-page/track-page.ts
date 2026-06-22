a23042d6df02dd937b49617ae83648a5b7b0a685
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 23:10:13 +0200

    feat: create track-page component

---
+++ src/app/pages/track-page/track-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-track-page',
+  imports: [],
+  templateUrl: './track-page.html',
+  styleUrl: './track-page.scss',
+})
+export class TrackPage {}
