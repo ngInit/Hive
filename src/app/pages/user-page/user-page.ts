773fc4d1f2d7a8d9313f5077ba57feabbe32a36c
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 10:17:08 +0200

    refactor: update imports in user page

--- src/app/pages/user-page/user-page.ts
+++ src/app/pages/user-page/user-page.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '@core/services/auth.service';
+import { FirebaseService } from '@core/services/firebase.service';
 
 @Component({
   selector: 'hive-user-page',
@@ -9,7 +9,7 @@ import { AuthService } from '@core/services/auth.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserPage {
-  private readonly authService = inject(AuthService);
+  private readonly authService = inject(FirebaseService);
   private readonly route = inject(ActivatedRoute);
   protected readonly pageTitle = String(this.route.snapshot.data['pageTitle']);
   protected readonly pageId = String(this.route.snapshot.paramMap.get('id'));
