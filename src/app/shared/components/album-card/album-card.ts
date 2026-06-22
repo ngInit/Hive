1bbaba65f534892709cda9c1e9b288e52b8a1a67
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 20:55:01 +0200

    feat: create album-card component

---
+++ src/app/shared/components/album-card/album-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-album-card',
+  imports: [],
+  templateUrl: './album-card.html',
+  styleUrl: './album-card.scss',
+})
+export class AlbumCard {}
