35a4ff842b4286b16e5757591dd950b256137748
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-22 23:45:08 +0200

    feat: add signals for track ID and Track

--- src/app/pages/track-page/track-page.ts
+++ src/app/pages/track-page/track-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Track } from '@core/models/jamendo/tracks.model';
+import { JamendoService } from '@core/services/jamendo.service';
 
 @Component({
   selector: 'hive-track-page',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './track-page.html',
   styleUrl: './track-page.scss',
 })
-export class TrackPage {}
+export class TrackPage implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly jamendoService = inject(JamendoService);
+  private readonly trackId = signal<string>('');
+  readonly track = signal<Track | null>(null);
+
+  ngOnInit(): void {
+    const queryId = String(this.route.snapshot.queryParamMap.get('q'));
+    this.trackId.set(queryId);
+    void this.jamendoService.getTrackPage(this.trackId()).then((response) => {
+      this.track.set(response.track);
+    });
+  }
+}
