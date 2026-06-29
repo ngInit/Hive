4fb1553b91531310cf4f23922c128832feac33ee
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-29 23:09:55 +0200

    refactor: change toggle method in player service

--- src/app/core/services/player.service.ts
+++ src/app/core/services/player.service.ts
@@ -1,31 +1,57 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Track } from '@core/models/jamendo/tracks.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
-  private readonly currentTrackId = signal<number | null>(null);
+  private readonly trackList = signal<Track[]>([]);
+  private readonly currentTrackIndex = signal<number>(0);
   private readonly isPlaying = signal<boolean>(false);
-
-  readonly playingTrackId = this.currentTrackId.asReadonly();
   readonly isPlayingTrack = this.isPlaying.asReadonly();
+  readonly playingTrack = computed<Track | null>(() => {
+    const tracks = this.trackList();
+    const index = this.currentTrackIndex();
+    return tracks[index] ?? null;
+  });
+  readonly hasNextTrack = computed(() => {
+    return this.currentTrackIndex() < this.trackList().length - 1;
+  });
+  readonly hasPreviousTrack = computed(() => {
+    return this.currentTrackIndex() > 0;
+  });
 
-  playTrack(track: Track): void {
-    this.currentTrackId.set(Number(track.id));
+  playCollection(tracks: Track[]): void {
+    if (tracks.length === 0) {
+      return;
+    }
+    this.trackList.set(tracks);
+    this.currentTrackIndex.set(0);
     this.isPlaying.set(true);
   }
 
+  playTrack(track: Track): void {
+    this.playCollection([track]);
+  }
+
   pauseTrack(): void {
-    this.currentTrackId.set(null);
     this.isPlaying.set(false);
   }
 
+  resumeTrack(): void {
+    this.isPlaying.set(true);
+  }
+
   toggle(track: Track): void {
-    if (this.currentTrackId() === Number(track.id)) {
+    const isTheSameTrack = this.playingTrack()?.id === track.id;
+    if (!isTheSameTrack) {
+      this.playTrack(track);
+      return;
+    }
+    if (this.isPlaying()) {
       this.pauseTrack();
     } else {
-      this.playTrack(track);
+      this.resumeTrack();
     }
   }
 }
