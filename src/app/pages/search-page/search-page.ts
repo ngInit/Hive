ed961564f89fd15bf3b8ae70330f17689d7750eb
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-06-30 13:04:44 +0200

    feat: add search-page component

---
+++ src/app/pages/search-page/search-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-search-page',
+  imports: [],
+  templateUrl: './search-page.html',
+  styleUrl: './search-page.scss',
+})
+export class SearchPage {}
