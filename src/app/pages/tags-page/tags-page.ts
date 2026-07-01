0f58f2401f7106cb7dee5a415fa22955ff4b38e9
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-01 10:11:06 +0200

    feat: add tags-page component

---
+++ src/app/pages/tags-page/tags-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-tags-page',
+  imports: [],
+  templateUrl: './tags-page.html',
+  styleUrl: './tags-page.scss',
+})
+export class TagsPage {}
