147ebf39e0a66abd6f5b82ef3a5a875becdf3bc9
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-01 09:42:41 +0200

    refactor: move artist card logic from search page to component

--- src/app/shared/components/artist-card/artist-card.ts
+++ src/app/shared/components/artist-card/artist-card.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { NavigationService } from '@core/services/navigation.service';
+import { Artist } from '@core/models/jamendo/artists.model';
 
 @Component({
   selector: 'hive-artist-card',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './artist-card.html',
   styleUrl: './artist-card.scss',
 })
-export class ArtistCard {}
+export class ArtistCard {
+  private readonly navigationService = inject(NavigationService);
+  readonly artist = input.required<Artist>();
+
+  goToArtist(id: string): void {
+    void this.navigationService.goToArtist(id);
+  }
+}
