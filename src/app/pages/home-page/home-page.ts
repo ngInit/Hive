b08ad2ecb6d5ab65eac55657336108258991de2b
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-01 10:15:20 +0200

    feat: add home-page component

---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-home-page',
+  imports: [],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.scss',
+})
+export class HomePage {}
