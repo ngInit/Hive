8376fb1ebf6cb5e85443adbe3aa9d04219f7e80b
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-01 09:25:42 +0200

    feat: add artist-card component

---
+++ src/app/shared/components/artist-card/artist-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-artist-card',
+  imports: [],
+  templateUrl: './artist-card.html',
+  styleUrl: './artist-card.scss',
+})
+export class ArtistCard {}
