59248c83a1046bbe17f486fb03d62d15cbf0f7f0
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-04 14:02:18 +0200

    feat: add album-page component

---
+++ src/app/pages/album-page/album-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-album-page',
+  imports: [],
+  templateUrl: './album-page.html',
+  styleUrl: './album-page.scss',
+})
+export class AlbumPage {}
