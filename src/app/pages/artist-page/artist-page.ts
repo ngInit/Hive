a7539e79b1db2750b81a9b3d05fca1c8502f9cf3
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-04 10:13:18 +0200

    feat: add artist-page component

---
+++ src/app/pages/artist-page/artist-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-artist-page',
+  imports: [],
+  templateUrl: './artist-page.html',
+  styleUrl: './artist-page.scss',
+})
+export class ArtistPage {}
