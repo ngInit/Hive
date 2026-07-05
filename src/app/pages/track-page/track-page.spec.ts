6c044d5d50eb1142091e01aed369fbfdae07050a
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-05 21:41:35 +0200

    feat: add tests for track page

--- src/app/pages/track-page/track-page.spec.ts
+++ src/app/pages/track-page/track-page.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideJamendoRepository } from '@core/providers/repository.providers';
+import { provideRouter } from '@angular/router';
 import { TrackPage } from './track-page';
+import { tracksMock } from '@shared/mocks/tracks.mock';
+
+const track = tracksMock.results[0];
 
 describe('TrackPage', () => {
   let component: TrackPage;
@@ -9,6 +13,7 @@ describe('TrackPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TrackPage],
+      providers: [provideJamendoRepository(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TrackPage);
@@ -16,7 +21,32 @@ describe('TrackPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Render track information', () => {
+    component.track.set(track);
+    fixture.detectChanges();
+
+    if (fixture.nativeElement instanceof HTMLElement) {
+      const links = fixture.nativeElement.querySelectorAll('.release-information p');
+      expect(links[0].textContent.trim()).toBe(`Released: ${track.releasedate}`);
+    } else {
+      throw new Error('Expected nativeElement to be HTMLElement');
+    }
+  });
+
+  it('Render error message', () => {
+    component.errorMessage.set(`Can't load this artist. Please try again later.`);
+    fixture.detectChanges();
+
+    if (fixture.nativeElement instanceof HTMLElement) {
+      expect(fixture.nativeElement.querySelector('.track-page-error')?.textContent).toBe(
+        `Can't load this artist. Please try again later.`
+      );
+    } else {
+      throw new Error('Expected nativeElement to be HTMLElement');
+    }
   });
 });
