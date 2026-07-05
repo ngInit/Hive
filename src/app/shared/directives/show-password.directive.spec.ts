569cfe44d2564886f2e2bca31ddd90dbedb3e59e
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-05 18:58:43 +0200

    feat: add tests to show password directive

--- src/app/shared/directives/show-password.directive.spec.ts
+++ src/app/shared/directives/show-password.directive.spec.ts
@@ -1,8 +1,53 @@
 import { ShowPassword } from './show-password.directive';
 
-describe('ShowPasswordDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowPassword();
+describe('ShowPassword directive', () => {
+  let directive: ShowPassword;
+
+  beforeEach(() => {
+    directive = new ShowPassword();
+  });
+
+  it('should create', () => {
     expect(directive).toBeTruthy();
+  });
+
+  it('Should start in hidden state', () => {
+    expect(directive.visible).toBe(false);
+  });
+
+  describe('Toggle visibility', () => {
+    it.each([
+      {
+        title: 'Toggle one time',
+        action: (directive: ShowPassword) => {
+          directive.toggle();
+        },
+        expected: true,
+      },
+      {
+        title: 'Toggle two times',
+        action: (directive: ShowPassword) => {
+          directive.toggle();
+          directive.toggle();
+        },
+        expected: false,
+      },
+    ])('$title', ({ action, expected }) => {
+      action(directive);
+      expect(directive.visible).toBe(expected);
+    });
+  });
+
+  describe('Call reset', () => {
+    it('Should be hidden', () => {
+      directive.toggle();
+      directive.reset();
+      expect(directive.visible).toBe(false);
+    });
+
+    it('Should be still hidden', () => {
+      directive.reset();
+      expect(directive.visible).toBe(false);
+    });
   });
 });
