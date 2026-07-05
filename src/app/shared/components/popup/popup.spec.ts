593b9ad25d29254febf6320d59c99935e6551fb9
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-05 22:04:53 +0200

    feat: add tests to popup component

--- src/app/shared/components/popup/popup.spec.ts
+++ src/app/shared/components/popup/popup.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { Popup } from './popup';
 
 describe('Popup', () => {
@@ -9,14 +9,45 @@ describe('Popup', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Popup],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Popup);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('open', true);
+    fixture.componentRef.setInput('userStatus', false);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Render guest menu items', () => {
+    fixture.componentRef.setInput('userStatus', false);
+    fixture.componentRef.setInput('open', true);
+    fixture.detectChanges();
+
+    if (fixture.nativeElement instanceof HTMLElement) {
+      const items = fixture.nativeElement.querySelectorAll('.popup-list-item');
+      expect(items[0].textContent.trim()).toBe('About');
+    } else {
+      throw new Error('Expected nativeElement to be HTMLElement');
+    }
+  });
+
+  it('Render user menu items', () => {
+    fixture.componentRef.setInput('userStatus', true);
+    fixture.componentRef.setInput('open', true);
+    fixture.detectChanges();
+
+    if (fixture.nativeElement instanceof HTMLElement) {
+      const items = fixture.nativeElement.querySelectorAll('.popup-list-item');
+      expect(items[0].textContent.trim()).toBe('Profile');
+      expect(items[1].textContent.trim()).toBe('About');
+      expect(items[2].textContent.trim()).toBe('Sign out');
+    } else {
+      throw new Error('Expected nativeElement to be HTMLElement');
+    }
   });
 });
