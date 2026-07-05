40d02cc1a2ca826efd73a69b39a6c88d776b9efc
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-05 21:53:17 +0200

    refactor: change tests for main layout

--- src/app/core/layouts/main-layout/main-layout.spec.ts
+++ src/app/core/layouts/main-layout/main-layout.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideAuthRepository } from '@core/providers/repository.providers';
+import { provideJamendoRepository } from '@core/providers/repository.providers';
+import { provideRouter } from '@angular/router';
 import { MainLayout } from './main-layout';
 
 describe('MainLayout', () => {
@@ -9,6 +11,7 @@ describe('MainLayout', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MainLayout],
+      providers: [provideAuthRepository(), provideJamendoRepository(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainLayout);
@@ -16,7 +19,7 @@ describe('MainLayout', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
     expect(component).toBeTruthy();
   });
 });
