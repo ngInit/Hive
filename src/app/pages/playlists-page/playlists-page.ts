935464c63f328bf972f0208404fd2365c7907af0
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-07 22:56:52 +0200

    feat: add playlist with liked tracks and tracklist container

--- src/app/pages/playlists-page/playlists-page.ts
+++ src/app/pages/playlists-page/playlists-page.ts
@@ -2,17 +2,21 @@ import { Component, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { Track } from '@core/models/jamendo/tracks.model';
 import { TrackPlaysShortPipe } from '@shared/pipes/track-plays-short-pipe';
+import { tracksMock } from '@shared/mocks/tracks.mock';
+import { NgTemplateOutlet } from '@angular/common';
+import { TrackCard } from '@components/track-card/track-card';
 
 @Component({
   selector: 'hive-playlists-page',
-  imports: [MatIcon, TrackPlaysShortPipe],
+  imports: [MatIcon, TrackPlaysShortPipe, NgTemplateOutlet, TrackCard],
   templateUrl: './playlists-page.html',
   styleUrl: './playlists-page.scss',
 })
 export class PlaylistsPage {
-  readonly likedTracks = signal<Track[]>([]);
+  readonly likedTracks = signal<Track[]>(tracksMock.results);
+  readonly tracklist = signal<Track[]>([]);
 
-  showLikedTracks(): void {
-    console.log('Show liked tracks');
+  showTracklist(): void {
+    this.tracklist.set(this.likedTracks());
   }
 }
