d0ab49090805b9bb0d7948c9578ab7850ad3651f
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-07 22:09:53 +0200

    feat: add base structure for liked tracks

--- src/app/pages/playlists-page/playlists-page.ts
+++ src/app/pages/playlists-page/playlists-page.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { Track } from '@core/models/jamendo/tracks.model';
+import { TrackPlaysShortPipe } from '@shared/pipes/track-plays-short-pipe';
 
 @Component({
   selector: 'hive-playlists-page',
-  imports: [],
+  imports: [MatIcon, TrackPlaysShortPipe],
   templateUrl: './playlists-page.html',
   styleUrl: './playlists-page.scss',
 })
-export class PlaylistsPage {}
+export class PlaylistsPage {
+  readonly likedTracks = signal<Track[]>([]);
+
+  showLikedTracks(): void {
+    console.log('Show liked tracks');
+  }
+}
