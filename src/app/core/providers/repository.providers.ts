ae17fc68a6432a384f617397cf8e98f2176f8334
ngInit <122630180+ngInit@users.noreply.github.com>
2026-07-08 21:56:25 +0200

    feat: add playlist service (#168)
    * refactor: move firebase app and db initialization to own directory in core
    
    * refactor: remove unused user converter
    
    * feat: add models for playlist
    
    * feat: add playlist repository abstract interface
    
    * feat: add FirestorePlaylistRepository for real data
    
    * feat: add playlist repository provider
    
    * feat: add playlist service
    
    (cherry picked from commit 103b7e16bd00c70236bbeb425cb16aeda8b44b34)

--- src/app/core/providers/repository.providers.ts
+++ src/app/core/providers/repository.providers.ts
@@ -1,8 +1,10 @@
 import { environment } from '@env/environment';
 import { AUTH_REPOSITORY } from '@core/repositories/firebase/firebase.repository';
 import { JAMENDO_REPOSITORY } from '@core/repositories/jamendo/jamendo.repository';
+import { PLAYLIST_REPOSITORY } from '@core/repositories/firestore/playlist.repository';
 import { FirebaseMockRepository } from '@core/repositories/firebase/firebase-mock.repository';
 import { FirebaseAuthRepository } from '@core/repositories/firebase/firebase-auth.repository';
+import { FirestorePlaylistRepository } from '@core/repositories/firestore/firestore-playlist.repository';
 import { JamendoMockRepository } from '@core/repositories/jamendo/jamendo-mock.repository';
 import { JamendoDbRepository } from '@core/repositories/jamendo/jamendo-db.repository';
 
@@ -20,6 +22,13 @@ export function provideJamendoRepository() {
   };
 }
 
+export function providePlaylistRepository() {
+  return {
+    provide: PLAYLIST_REPOSITORY,
+    useClass: FirestorePlaylistRepository,
+  };
+}
+
 export function provideRepositories() {
-  return [provideJamendoRepository(), provideAuthRepository()];
+  return [provideJamendoRepository(), provideAuthRepository(), providePlaylistRepository()];
 }
