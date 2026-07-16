6f7d049238e3bbb82cd0b51e6384fd8732af6066
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-07-16 20:06:05 +0200

    feat: add tracks to track list

--- src/app/pages/playlists-page/playlists-page.ts
+++ src/app/pages/playlists-page/playlists-page.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, effect } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { Track } from '@core/models/jamendo/tracks.model';
 import { TrackPlaysShortPipe } from '@shared/pipes/track-plays-short-pipe';
 import { tracksMock } from '@shared/mocks/tracks.mock';
 import { NgTemplateOutlet } from '@angular/common';
 import { TrackCard } from '@components/track-card/track-card';
+import { PlaylistService } from '@core/services/playlist.service';
+import { Playlist } from '@core/models/playlist.model';
 
 @Component({
   selector: 'hive-playlists-page',
@@ -13,10 +15,21 @@ import { TrackCard } from '@components/track-card/track-card';
   styleUrl: './playlists-page.scss',
 })
 export class PlaylistsPage {
+  readonly playlistsService = inject(PlaylistService);
   readonly likedTracks = signal<Track[]>(tracksMock.results);
   readonly tracklist = signal<Track[]>([]);
+  readonly lists = signal<Playlist[]>([]);
 
-  showTracklist(): void {
+  constructor() {
+    effect(() => {
+      void this.playlistsService.loadAllPlaylists();
+      this.lists.set(this.playlistsService.playlists());
+    });
+  }
+
+  async showTracklist(): Promise<void> {
     this.tracklist.set(this.likedTracks());
+    await this.playlistsService.loadAllPlaylists();
+    this.lists.set(this.playlistsService.playlists());
   }
 }
